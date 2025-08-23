d3d3f99c8725febf3276848a6c557c73a8be5bad
unknown <yashkolnure58@gmail.com>
2025-08-23 13:59:03 +0530

    Initial commit

--- src/pages/loginfree.js
+++ src/pages/loginfree.js
@@ -11,7 +11,7 @@ function Loginfree() {
     setError("");
     try {
       const res = await fetch(
-        "http://168.231.123.91:5000/api/api/admin/login",
+        "/api/admin/login",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
